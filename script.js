215c5ebb5cfae97a36b3ed9e24ef286f2d5ed4cc
Subhash Chamara <udayanga.subhash94@gmail.com>
2025-01-05 23:22:11 +0530

    stykes sheet seperate

---
+++ script.js
@@ -0,0 +1,5 @@
+// Splash Screen Timer
+setTimeout(function () {
+    document.getElementById('splash-screen').classList.add('hidden');
+    document.getElementById('main-content').style.display = 'block';
+}, 3000); // 3 seconds
